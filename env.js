a3c392ef15c019d0320f530118e3c3edfa644ac2
Giovanna Hollo <116689299+GiovannaHollo@users.noreply.github.com>
2026-04-08 17:25:36 -0300

    Delete env.js

--- env.js
+++
@@ -1,6 +0,0 @@
-// Este arquivo é GERADO automaticamente pelo build do Vercel.
-// Não edite manualmente. Veja vercel.json para entender como funciona.
-window.__env = {
-  SUPABASE_URL:      "%%SUPABASE_URL%%",
-  SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
-};
